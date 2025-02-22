28e8ece21ee963a5a55f70356485fc7e55509bc5
Werbeth Araujo <werbeth1991pbs@gmail.com>
2025-02-22 17:44:05 -0300

    chore: update css global

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ const Home = () => {
   return (
     <div className="p-5 border border-blue-700 rounded-xl">
       <h1>Home Page</h1>
-      <Button className="w-10 h-10 px-10 py-5 bg-blue-700 text-white">
-        Iniciar
-      </Button>
+      <Button>Iniciar Projeto</Button>
     </div>
   );
 };
