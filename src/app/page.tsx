ab38195c92312ab5bad7e1e42eae0f22b2423fd6
Werbeth Araujo <werbeth1991pbs@gmail.com>
2025-02-22 17:35:32 -0300

    feat: add shadcn setup and component button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="p-5 border border-blue-700 rounded-xl">
+      <h1>Home Page</h1>
+      <Button className="w-10 h-10 px-10 py-5 bg-blue-700 text-white">
+        Iniciar
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
